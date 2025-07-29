49fe14b61e48fb5eaa00211842d926d86a6931bb
DaaaPaul <paulpeng2008@gmail.com>
2025-07-29 16:19:22 -0400

    Add project files.

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main() {
+    std::cout << "HELLO GRAPHICS PROGRAMMING ON WINDOWS!!!!" << std::endl;
+    
+    return 0;
+}
