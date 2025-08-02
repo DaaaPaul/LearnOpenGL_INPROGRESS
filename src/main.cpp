95a98a4496241da40e95415725b121c8f01b2f1e
DaaaPaul <paulpeng2008@gmail.com>
2025-08-02 13:39:29 -0400

    Add glad.c to project files

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,30 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+void windowSizeAdjustCallback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
 int main() {
-    std::cout << "HELLO GRAPHICS PROGRAMMING ON WINDOWS!!!!" << std::endl;
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+    GLFWwindow* window = glfwCreateWindow(800, 800, "LearnOpenGL", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "GLFWwindow object creation failed" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "GLAD setup failed" << std::endl;
+    }
+    
+    glViewport(0, 0, 800, 800);
+    glfwSetFramebufferSizeCallback(window, windowSizeAdjustCallback);
     
     return 0;
 }
